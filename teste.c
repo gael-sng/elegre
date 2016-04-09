6e257013cef9f7fba27b9b7a03927bc41ca17454
Gabriel SN <gael.sng@gmail.com>
2016-04-09 16:08:59 -0300

    sayHI

--- teste.c
+++ teste.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+void sayHI(){
+	printf("HI\n");
+}
 
 int main (int argc, char const *argv[]){
-	
+	sayHI();
 	int i;
 	for(i=0;i< argc ;i++){
 		printf("parametro %d : %s\n", i, argv[i]);
