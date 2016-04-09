468a4a3eab5ad28fa3ed16e940937afc3009b1f3
Gabriel SN <gael.sng@gmail.com>
2016-04-09 15:59:03 -0300

    coloquei o fatorial loko

--- teste.c
+++ teste.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+int fatorial(int n){
+	if(n == 0)return 1;
+	return n * fatorial(n-1);
+}
 
 int main (int argc, char const *argv[]){
 	
@@ -9,6 +12,8 @@ int main (int argc, char const *argv[]){
 	for(i=0;i< argc ;i++){
 		printf("parametro %d : %s\n", i, argv[i]);
 	}
+
+	printf("fatorial de %d é %d\n", argc, fatorial(argc));
 
 	return 0;
 }
